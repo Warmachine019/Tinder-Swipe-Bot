65c21cd54886584719eb58619748702ff647ba19
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-01-16 22:32:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,76 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-import time
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
 
+FB_EMAIL = "EMAIL ADDRESS USED TO LOG INTO FACEBOOK"
+FB_PASSWORD = "PASSWORD USED TO LOG INTO FACEBOOK"
+
+#To prevent chrome from shutting down
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://tinder.com/")
+driver.get("http://www.tinder.com")
 
-time.sleep(2)
-login_button_1 = driver.find_element(by=By.CLASS_NAME, value='lxn9zzn')
-login_button_1.click()
+sleep(2)
+login_button = driver.find_element(By.XPATH, value='//*[text()="Log in"]')
+login_button.click()
 
-time.sleep(2)
-more_options_button = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/button')
-login_button_1.click()
+# try:
+#     sleep(2)
+#     more_options_button = driver.find_element(By.CLASS_NAME, value='Td(u) Cur(p) Typs(button-2) focus-outline-style')
+#     more_options_button.click()
+# except NoSuchElementException:
+#     sleep(1)
 
-time.sleep(2)
-facebook_button = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]')
-facebook_button.click()
+sleep(2)
+fb_login = driver.find_element(By.XPATH,value='/html/body/div[2]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]')
+fb_login.click()
+
+tinder_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+sleep(2)
+email_field = driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[1]/form/div/div[1]/div/input")
+email_field.send_keys(FB_EMAIL)
+
+password_field = driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[1]/form/div/div[2]/div/input")
+password_field.send_keys(FB_PASSWORD)
+
+fb_login_button = driver.find_element(by=By.XPATH, value="/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]/input")
+fb_login_button.click()
+
+input("Press enter after completing captcha")
+
+driver.switch_to.window(tinder_window)
+print(driver.title)
+
+sleep(5)
+allow_location = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div[3]/button[1]/div[2]/div[2]")
+allow_location.click()
+
+sleep(5)
+no_notifs = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div/div/div[3]/button[2]/div[2]/div[2]")
+no_notifs.click()
+
+sleep(5)
+cookies = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]/div")
+cookies.click()
+
+for n in range(100):
+    sleep(2)
+    try:
+        print("called")
+        like_button = driver.find_element(By.XPATH, value='/html/body/div[1]/div/div[1]/div/div/div/main/div/div/div/div/div[4]/div/div[4]/button/span/span[1]')
+        like_button.click()
+
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element(By.CSS_SELECTOR, value=".itsAMatch a")
+            match_popup.click()
+
+        except NoSuchElementException:
+            sleep(2)
